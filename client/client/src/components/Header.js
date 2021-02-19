a84feffde74ea7e73509a517abd8ea7340389437
Maria Patokina <mpatokina@hotmail.com>
2021-02-19 14:53:02 -0500

    updated shelters component

--- client/client/src/components/Header.js
+++ client/client/src/components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import catanddog from './catanddog.png';
+import animals from './animals.png';
 
 class Header extends React.Component {
     render() {
         return(
             <div className="header-name">
                 <h1>AnimalCare</h1>
-                <img src={ catanddog } alt="cat and dog" />
+                <img src={ animals } alt="cat and dog" />
             </div>
         )
     }
